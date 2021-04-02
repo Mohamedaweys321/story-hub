549998d5ad98af8008e55559a91b01d55bde32db
Mohamedaweys321 <71419130+Mohamedaweys321@users.noreply.github.com>
2021-04-02 13:03:52 -0700

    Add files via upload

---
+++ screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import {Text, View, StyleSheet, Button, TouchableOpacity} from 'react-native';
+
+export default class SearchScreen extends React.Component {
+    render() {
+        return (
+            <Text style = {{
+                alignSelf: 'center',
+                marginTop: 200
+            }}>Search</Text>
+        );
+    }
+}
